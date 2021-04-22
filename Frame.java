e21d42381750b861e8ce2401a96f8687cb8e3717
junhoYu <joun0106@gmail.com>
2021-04-22 12:14:03 +0900

    프레임 파일 입니다.

---
+++ Frame.java
@@ -0,0 +1,15 @@
+import java.awt.Frame;
+
+public class Frame {
+	// 메인
+	
+	public static void main(String[] args) {
+		
+		Frame f= new Frame();	// f라는 Frame 생성
+		f.setTitle("First Frame");	//창 제목 설정
+		f.setBounds(100, 100, 300, 300);	//창 크기 설정
+		//		   (x, y, width, height)
+		f.setVisible(true);	//창보이기 값 설정
+	}
+
+}
